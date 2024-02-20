54d9adc3ddd6af564882f0393046cbc75a0b58fa
Cleo <46943386+chfal@users.noreply.github.com>
2024-02-20 15:42:07 -0500

    Create rename.sh

---
+++ astral/rename.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sed -i 's/changed_lizard_adv_2_lizard_adv_2/LizardAdv1/g' output_tree.tre
+sed -i 's/changed_odocoileus_adv_1_odocoileus_adv_1/OdocoileusAdv1/g' output_tree.tre
+sed -i 's/changed_bovine_adv_d_bovine_adv_d/BovineAdvD/g' output_tree.tre
+sed -i 's/changed_bearded_dragon_adv_1_bearded_dragon_adv_1/BeardedDragonAdv1/g' output_tree.tre
+sed -i 's/changed_white_sturgeon_adv_1_white_sturgeon_adv_1/WhiteSturgeonAdv1/g' output_tree.tre
+sed -i 's/changed_duck_adv_a_duck_adv_a/DuckAdvA/g' output_tree.tre
+
